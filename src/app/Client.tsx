820a59729385ae33e7aea2309098cc96381d0a83
Marco Ingraiti <info@marcoingraiti.it>
2024-04-05 13:26:28 +0200

    feat: added test client component

---
+++ src/app/Client.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Client()
+{
+    console.log("Hello from client.tsx 2");
+
+    return (
+        <div>
+            <h1>Hello from client.tsx DOM</h1>
+        </div>
+    );
+}
